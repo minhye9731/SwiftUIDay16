5db5b32ef99d778f5577e424e69e23911aa1fa15
minhye9731 <jenney62639731@gmail.com>
2023-09-24 01:05:17 +0900

    feat : configure NavigationView, Section, Picker, ForEach with @State property

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let students = ["Harry", "Hermione", "Ron"]
+    
+    @State private var tapCount = 0
+    @State private var name = ""
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Button("Tap Count: \(tapCount)") {
+                    self.tapCount += 1
+                }
+                
+                Section {
+                    TextField("Enter your name", text: $name)
+                    Text("Your name is \(name)")
+                }
+                
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                Section {
+                    Text("(1) Hello, world!")
+                    Text("(2) Hello, world!")
+                    Text("(3) Hello, world!")
+                    Text("(4) Hello, world!")
+                    Text("(5) Hello, world!")
+                    Text("(6) Hello, world!")
+                }
+                
+                Section {
+                    Text("(7) Hello, world!")
+                    Text("(8) Hello, world!")
+                    Text("(9) Hello, world!")
+                    Text("(10) Hello, world!")
+                    Text("(11) Hello, world!")
+                }
+            }
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.automatic)
         }
-        .padding()
     }
 }
 
