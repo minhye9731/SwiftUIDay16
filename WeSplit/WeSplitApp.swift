537439e73da425bc5877e689d191e51d2ca90820
minhye9731 <jenney62639731@gmail.com>
2023-09-23 15:42:33 +0900

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by 강민혜 on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
