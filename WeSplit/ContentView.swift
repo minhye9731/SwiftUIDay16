ff517be24b4ad117039607408ff381781d59cc56
minhye9731 <jenney62639731@gmail.com>
2023-09-27 16:05:54 +0900

    [DAY 17] feat : apply textfield, segmented control, @FocusState, toolbar, toolbaritemgroup, spacer

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -15,43 +15,103 @@ struct ContentView: View {
     @State private var name = ""
     @State private var selectedStudent = "Harry"
     
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        // calculate the total per person here
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationView {
             Form {
-                Button("Tap Count: \(tapCount)") {
-                    self.tapCount += 1
+                
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused) // 해당 Textfield에 amountIsFocused 상태값을 적용
+
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+
                 }
                 
                 Section {
-                    TextField("Enter your name", text: $name)
-                    Text("Your name is \(name)")
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip fo you want to leave?")
                 }
                 
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                
+//                Button("Tap Count: \(tapCount)") {
+//                    self.tapCount += 1
+//                }
+//
+//                Section {
+//                    TextField("Enter your name", text: $name)
+//                    Text("Your name is \(name)")
+//                }
+//
+//                Picker("Select your student", selection: $selectedStudent) {
+//                    ForEach(students, id: \.self) {
+//                        Text($0)
+//                    }
+//                }
+//
+//                Section {
+//                    Text("(1) Hello, world!")
+//                    Text("(2) Hello, world!")
+//                    Text("(3) Hello, world!")
+//                    Text("(4) Hello, world!")
+//                    Text("(5) Hello, world!")
+//                    Text("(6) Hello, world!")
+//                }
+//
+//                Section {
+//                    Text("(7) Hello, world!")
+//                    Text("(8) Hello, world!")
+//                    Text("(9) Hello, world!")
+//                    Text("(10) Hello, world!")
+//                    Text("(11) Hello, world!")
+//                }
+            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.automatic)
+            
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
                     }
                 }
-                
-                Section {
-                    Text("(1) Hello, world!")
-                    Text("(2) Hello, world!")
-                    Text("(3) Hello, world!")
-                    Text("(4) Hello, world!")
-                    Text("(5) Hello, world!")
-                    Text("(6) Hello, world!")
-                }
-                
-                Section {
-                    Text("(7) Hello, world!")
-                    Text("(8) Hello, world!")
-                    Text("(9) Hello, world!")
-                    Text("(10) Hello, world!")
-                    Text("(11) Hello, world!")
-                }
             }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.automatic)
         }
     }
 }
